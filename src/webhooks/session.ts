bfde05a1436db67d4834392c34e02f87d3499ee3
Sugeng Riyanto PC <ugikpoenya@gmail.com>
2025-11-12 16:11:41 +0700

    session webhook send detail

--- src/webhooks/session.ts
+++ src/webhooks/session.ts
@@ -1,19 +1,33 @@
 import { CreateWebhookProps, webhookClient } from ".";
+import * as whastapp from "wa-multi-session";
 
 type SessionStatus = "connected" | "disconnected" | "connecting";
 
 type WebhookSessionBody = {
   session: string;
   status: SessionStatus;
+  number: string;
+  name: string;
+  id: string;
+  lid: string;
 };
 
 export const createWebhookSession =
   (props: CreateWebhookProps) => async (event: WebhookSessionBody) => {
     const endpoint = `${props.baseUrl}/session`;
 
+    const info = whastapp.getSession(event.session);
+    const name = info?.user?.name ?? "";
+    const jid = info?.user?.id ?? "";
+    const number = jid.split(/[:@]/)[0];
+
     const body = {
       session: event.session,
       status: event.status,
+      number: number ?? "",
+      name: name,
+      id: info?.user?.id ?? "",
+      lid: info?.user?.lid ?? "",
     } satisfies WebhookSessionBody;
 
     webhookClient.post(endpoint, body).catch(console.error);
